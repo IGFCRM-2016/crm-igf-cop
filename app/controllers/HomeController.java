91b188c0d99c0bb934722ec8e84128226753d50d
RuthTorrentoLara <rstorrento.25@gmail.com>
2016-11-26 20:29:39 -0600

    mostrar productos y ofertas Listo.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -203,4 +203,35 @@ public class HomeController extends Controller {
 
     }
 
+
+    public Result getOfferImage(Long id){
+
+        try {
+            Oferta offer = Oferta.find.byId(id);
+            InputStream input = new ByteArrayInputStream(offer.imagen);
+            java.awt.image.BufferedImage image =  javax.imageio.ImageIO.read(input);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            javax.imageio.ImageIO.write(image,offer.contentTypeImagen.split("/")[1],baos);
+
+            return ok(baos.toByteArray()).as(offer.contentTypeImagen);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        return ok();
+    }
+    /*Paginador para ofertas*/
+     public Result getOfferPage(Long page){
+        if (page<0){
+            return redirect(routes.HomeController.getOfferPage(0));
+        }
+       int page_size=9;
+       List<Oferta> offer = Oferta.find.orderBy("id").findPagedList(page.intValue(),page_size).getList();
+       List<Categoria> categorias_list = Categoria.find.findList();
+       
+       return ok(ofertas.render(offer,categorias_list,page));
+        
+
+    }
 }
